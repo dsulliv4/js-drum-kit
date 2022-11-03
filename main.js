0b9a6f572b4696f8dd4d95c13d24e17f414360e3
Deirdre Sullivan <drsullivan93@gmail.com>
2022-11-03 02:28:13 -0400

    audio for keys

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 window.addEventListener('keydown', function(e) {
-    console.log(e);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //stop function from running
+    audio.currentTime = 0; //rewind to the start
+    audio.play;
+    
 })
